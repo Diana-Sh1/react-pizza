325edd6b3de6125ff51bfef79ebeb4b58a44b6fa
Diana-Sh1 <dianaa15052015@gmail.com>
2023-07-21 12:38:57 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import '../src/scss/app.scss';
 import Header from "./Components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import {Route, Routes} from "react-router-dom";
+import Cart from "./pages/Cart";
 
 function App() {
 
@@ -11,7 +13,11 @@ function App() {
             <Header/>
             <div className="content">
                 <div className="container">
-                    <NotFound/>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/cart" element={<Cart/>} />
+                        <Route path="*" element={<NotFound/>} />
+                    </Routes>
                 </div>
             </div>
         </div>
--- src/Components/NotFoundBlock/index.jsx
+++ src/Components/NotFoundBlock/index.jsx
@@ -2,10 +2,11 @@ import s from './NotFound.module.scss'
 
 function NotFoundBlock() {
     return (
-        <div>
-            <span className={s.root}>😕</span>
-            <br/>
-            <h1>Ничего не найдено :( </h1>
+        <div className={s.root}>
+            <h1 >
+                <span>😕</span>
+            </h1>
+            <h1>Ничего не найдено</h1>
         </div>
     );
 }
