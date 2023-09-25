f5b29c2ea3b8e0b842a23fafc7ca79f8b11e1f22
Diana-Sh1 <dianaa15052015@gmail.com>
2023-09-25 11:34:25 +0300

    sorting&searching

--- src/Components/Sort.jsx
+++ src/Components/Sort.jsx
@@ -1,14 +1,15 @@
 import {useState} from "react";
 
-function Sort({value, onChangeSort}) {
+function Sort({value, onChangeSort, setOrderType, orderType}) {
+
     const [open, setOpen] = useState(false);
 
     const list = [
-        {name: 'популярности', sort: 'rating'},
-        {name: 'цене', sort: 'price'},
-        {name: 'алфавиту', sort: 'title'}
+        {name: 'популярности', sortProperty: 'rating'},
+        {name: 'цене', sortProperty: 'price'},
+        {name: 'алфавиту', sortProperty: 'title'}
     ];
-    const sortName = list[value].name
+
     const onClickSortItem = (i)=> {
         onChangeSort(i)
         setOpen(false)
@@ -17,26 +18,24 @@ function Sort({value, onChangeSort}) {
     return (
         <div className="sort">
             <div className="sort__label">
-                <svg
-                    width="10"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                        fill="#2C2C2C"
-                    />
-                </svg>
+
                 <b>Сортировка по:</b>
-                <span onClick={()=> setOpen(!open)}>{sortName}</span>
+                <span onClick={()=> setOpen(!open)}>{value.name}</span>
+                <div className="sort__buttons">
+                    <button onClick={() => setOrderType('asc')}> ↑ </button>
+                    <button onClick={() => setOrderType('desc')}> ↓ </button>
+                </div>
             </div>
+
+
             {open && (
                 <div className="sort__popup">
                     <ul>
-                        {list.map((name, i)=> <li onClick={()=> onClickSortItem(i)}  key={name}
-                                                   className={value === i? 'active': ''}>{list.name}
+                        {list.map((obj, i)=>
+                            <li
+                                key={i}
+                                onClick={()=> onClickSortItem(obj)}
+                                className={value.sortProperty === obj.sortProperty? 'active': ''}>{obj.name}
                         </li>)}
                     </ul>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,25 +9,31 @@ function Home() {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
     const [categoryId, setCategoryId] = useState(0);
-    const [sortType, setSortType] = useState(0);
+    const [orderType, setOrderType] = useState('asc');
+    const [sortType, setSortType] = useState({
+        name: 'популярности',
+        sortProperty: 'rating'
+    });
 
     useEffect(() => {
         setIsLoading(true);
-        fetch('https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?category=' + categoryId)
+
+        fetch(`https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=${orderType}`)
             .then(res => res.json())
             .then((arr) => {
                 setItems(arr);
                 setIsLoading(false);
             })
         window.scrollTo(0,0)
-    }, [categoryId])
+    }, [categoryId, sortType, orderType])
     return (
         <>
             <div className="content__top">
                 <Categories value={categoryId} onChangeCategory={(i)=> setCategoryId(i)}/>
-                <Sort value={sortType} onChangeSort={(i)=> setSortType(i)}/>
+                <Sort value={sortType} onChangeSort={(i)=> setSortType(i)} setOrderType={setOrderType} orderType/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
+
             <div className="content__items">
                 {isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>) : items.map(obj => <PizzaBlock
                     key={obj.id} {...obj}/>)
