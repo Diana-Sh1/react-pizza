baf98c9fa4c6130c7fe79006007daf5360f7247a
Diana-Sh1 <dianaa15052015@gmail.com>
2023-09-29 10:14:11 +0300

    pagination

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -4,8 +4,8 @@ import style from "./Search.module.scss";
 const Search = ({searchValue, setSearchValue}) => {
     return (
        <div className={styles.root}>
-           <svg className={styles.icon} fill="none" height="24" stroke="currentColor" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24"
+           <svg className={styles.icon} fill="none" height="24" stroke="currentColor" strokeLinecap="round"
+                strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24"
                 xmlns="http://www.w3.org/2000/svg">
                <circle cx="11" cy="11" r="8"/>
                <line x1="21" x2="16.65" y1="21" y2="16.65"/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import Categories from "../Components/Categories";
 import Sort from "../Components/Sort";
 import Skeleton from "../Components/PizzaBlock/Skeleton";
 import PizzaBlock from "../Components/PizzaBlock/PizzaBlock";
+import ReactPaginate from "react-paginate";
 import {useEffect, useState} from "react";
+import Pagination from "../Components/Pagination";
 
 
 function Home({searchValue}) {
@@ -17,15 +19,16 @@ function Home({searchValue}) {
 
     useEffect(() => {
         setIsLoading(true);
+        const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=${orderType}`)
+        fetch(`https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=${orderType}${search}`)
             .then(res => res.json())
             .then((arr) => {
                 setItems(arr);
                 setIsLoading(false);
             })
-        window.scrollTo(0,0)
-    }, [categoryId, sortType, orderType])
+        window.scrollTo(0, 0)
+    }, [categoryId, sortType, orderType, searchValue])
 
     const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj}/>)
     const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
@@ -33,8 +36,8 @@ function Home({searchValue}) {
     return (
         <>
             <div className="content__top">
-                <Categories value={categoryId} onChangeCategory={(i)=> setCategoryId(i)}/>
-                <Sort value={sortType} onChangeSort={(i)=> setSortType(i)} setOrderType={setOrderType} orderType/>
+                <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)}/>
+                <Sort value={sortType} onChangeSort={(i) => setSortType(i)} setOrderType={setOrderType} orderType/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
 
@@ -44,6 +47,7 @@ function Home({searchValue}) {
                     : pizzas
                 }
             </div>
+            <Pagination/>
         </>
     )
 }
