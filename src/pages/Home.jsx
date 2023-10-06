10a1b67898602c67c1faca20463f4aeaa98a3c6a
Diana-Sh1 <dianaa15052015@gmail.com>
2023-10-06 11:40:43 +0300

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,20 @@ import Categories from "../Components/Categories";
 import Sort from "../Components/Sort";
 import Skeleton from "../Components/PizzaBlock/Skeleton";
 import PizzaBlock from "../Components/PizzaBlock/PizzaBlock";
-import ReactPaginate from "react-paginate";
 import {useContext, useEffect, useState} from "react";
 import Pagination from "../Components/Pagination";
 import {SearchContext} from "../App";
 import {useDispatch, useSelector} from "react-redux"
 import {setCategoryId} from "../redux/slices/filterSlice";
+import axios from "axios";
 
 
 function Home() {
     const dispatch = useDispatch();
     const { categoryId, sort, order } = useSelector(state => state.filter)
-    // const sortType = useSelector(state => state.filter.sort.sortProperty)
-    // const orderType = useSelector(state => state.filter.order)
-
     const {searchValue} = useContext(SearchContext)
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
-
-    // const [orderType, setOrderType] = useState('asc');
     const [currentPage, setCurrentPage] = useState(1);
 
     const onChangeCategory = (id) => {
@@ -31,10 +26,9 @@ function Home() {
         setIsLoading(true);
         const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?page=${currentPage}&limit=4&${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sort.sortProperty}&order=${order}${search}`)
-            .then(res => res.json())
-            .then((arr) => {
-                setItems(arr);
+        axios.get(`https://64b513c6f3dbab5a95c6a8b7.mockapi.io/items?page=${currentPage}&limit=4&${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sort.sortProperty}&order=${order}${search}`)
+            .then(res => {
+                setItems(res.data);
                 setIsLoading(false);
             })
         window.scrollTo(0, 0)
